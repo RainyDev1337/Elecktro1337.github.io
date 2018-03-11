7837e6879a364ead45e0a88b6c0d8f1a1583d3d0
unknown <C:\Users\Elecktro1337\AppData\Roaming\The Bat!>
2018-03-11 19:19:26 +0300

    123

--- devsoftshop/js/1.js
+++ devsoftshop/js/1.js
@@ -1,90 +1,110 @@
-var app = {};
-app.ajax = 4;
-app.ui = {
-    particle:false,
-    particles:function(){
-        if($('body').hasClass('white')){
-            nb = 85;
-        }else {
-            nb = 95;
-        }
-        particlesJS('particles-js', {
-            particles: {
-                color: '#E6E6FA',
-                color_random: false,
-                shape: 'circle', // "circle", "edge" or "triangle"
-                opacity: {
-                    opacity: 0.8,
-                    anim: {
-                        enable: true,
-                        speed: 1.5,
-                        opacity_min: 0,
-                        sync: false
-                    }
-                },
-                size: 2,
-                size_random: true,
-                nb: nb,
-                line_linked: {
-                    enable_auto: true,
-                    distance: 130,
-                    color: '#E6E6FA', // particles line color
-                    opacity: 1,
-                    width: 1,
-                    condensed_mode: {
-                        enable: true,
-                        rotateX: 600,
-                        rotateY: 600
-                    }
-                },
-                anim: {
-                    enable: true,
-                    speed: 2
-                }
-            },
-            interactivity: {
-                enable: true,
-                mouse: {
-                    distance: 500
-                },
-                detect_on: 'window', // "canvas" or "window"
-                mode: 'grab', // "grab" of false
-                line_linked: {
-                    opacity: .5
-                },
-                events: {
-                    onclick: {
-                        enable: true,
-                        mode: 'push', // "push" or "remove"
-                        nb: 2
-                    },
-                    onresize: {
-                        enable: true,
-                        mode: 'out', // "out" or "bounce"
-                        density_auto: false,
-                        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
-                    }
-                }
-            },
-            /* Retina Display Support */
-            retina_detect: true
-        });
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 355,
+      "density": {
+        "enable": true,
+        "value_area": 789.1476416322727
+      }
     },
-}
-
-$(function () {
-    app.ui.particles();
-    if (Modernizr.localstorage) {
-        if(localStorage.getItem("color") == "white") {
-            $('body').addClass('white');
-        }
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.48927153781200905,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 0.2,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
     }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 83.91608391608392,
+        "size": 1,
+        "duration": 3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 });
-
-var delay = (function(){
-    var timer = 0;
-    return function(callback, ms){
-        clearTimeout (timer);
-        timer = setTimeout(callback, ms);
-    };
-})();
